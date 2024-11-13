42511077eeed5bef13a2091d07a90036e142554d
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-11-13 22:16:01 +0100

    ✨ Login : create Inscription form

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./components/pages/login/LoginPage";
+
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -1,5 +1,27 @@
+import { useForm } from "react-hook-form"
+import { yupSchema } from "./yupSchema"
+import { yupResolver } from "@hookform/resolvers/yup";
+
+
+type FormValues = {
+  email: string;
+  password: string;
+  verifyPassword: string;
+}
 export default function LoginPage() {
+  const { register, handleSubmit, formState: { errors } } = useForm<FormValues>({resolver: yupResolver(yupSchema)})
+
+  const onSubmit = (data : FormValues) => console.log(data) 
   return (
-    <div>LoginPage</div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("email")} placeholder="email" />
+      {errors.email && <p>{errors.email.message}</p>}
+      <input {...register("password")} placeholder="password" />
+      {errors.password && <p>{errors.password.message}</p>}
+      <input {...register("verifyPassword")} placeholder="verifyPassword" />
+      {errors.verifyPassword && <p>{errors.verifyPassword.message}</p>}
+
+      <button type="submit">Submit</button>
+    </form>
   )
 }
