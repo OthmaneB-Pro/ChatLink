f4a0e23350713c98c34227a8fa00aa5deacafc2e
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-11-16 13:39:11 +0100

    ✨ T02 : Menu > Create form to update username, password and picture

--- src/components/pages/menu/Menu.tsx
+++ src/components/pages/menu/Menu.tsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import { useUserStore } from "../../../store/useUserStore";
 
 export default function Menu() {
-  const { username, password, picture } = useUserStore();
-  return <div>Menu : 
+  const { username, password, picture, setUser } = useUserStore();
+  const [isModify, setIsModify] = useState(false);
 
-    <h2>Username : {username}</h2>
-    <h2>Picture : {picture}</h2>
-  </div>;
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setUser({ [name]: value });
+  };
+
+  return (
+    <div>
+      Menu :<h2>Username : {username}</h2>
+      <h2>
+        Picture : <img src={picture} alt="avatar" />
+      </h2>
+      <button onClick={() => setIsModify(true)}>Modifier mon profil</button>
+      {isModify && (
+        <form>
+          <input onChange={handleChange} value={username} name="username" />
+          <input onChange={handleChange} value={password} name="password" />
+          <input onChange={handleChange} value={picture} name="picture" />
+          <button type="submit" onClick={() => setIsModify(false)}>
+            Valider les modifications
+          </button>
+        </form>
+      )}
+    </div>
+  );
 }
