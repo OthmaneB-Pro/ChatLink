dacd24c9aff250c225d91bdf2613b11363768a14
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-11-17 15:40:33 +0100

    ✨ T04 : Crud > Create > New message with current time and texte write in form

--- src/components/pages/messaging/message/MainMessage.tsx
+++ src/components/pages/messaging/message/MainMessage.tsx
@@ -1,8 +1,37 @@
+import { useState } from "react";
 import Message from "../../../reusable-ui/Message";
+import { getCurrentTime } from "../../../../utils/getCurrentTime";
 
 export default function MainMessage() {
-  return <div className="messages">
+  const [message, setMessage] = useState<{ text: string; timestamp: string }[]>([]);
+  const [currentMessage, setCurrentMessage] = useState("");
 
-    <Message label="Hello OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO OOOOOOOOOOOOOOOOOOOOOOOOOO" timestamp="Today" />
-  </div>;
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (currentMessage.trim() === "") return;
+    const timestamp = getCurrentTime(); 
+    const newMessage = { text: currentMessage, timestamp };
+    
+    setMessage([...message, newMessage]); 
+    setCurrentMessage("");
+  };
+  return (
+    <div className="messages">
+      {message.map((message, index) => (
+        <Message label={message.text} key={index} timestamp={message.timestamp} />
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="message"
+          value={currentMessage}
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+        />
+        <button type="submit">OK</button>
+      </form>
+    </div>
+  );
 }
