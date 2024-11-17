fe183dcef97b6aa43b2e3e328d12753c345183d0
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-11-17 14:40:16 +0100

    ✨ T04 : route for messaging

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./components/pages/login/LoginPage";
 import ErrorPage from "./components/pages/error/ErrorPage";
 import Menu from "./components/pages/menu/Menu";
+import Messaging from "./components/pages/messaging/Messaging";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Routes>
           <Route path={"*"} element={<ErrorPage />} />
           <Route path={"/"} element={<LoginPage />} />
+          <Route path={"/messaging"} element={<Messaging />} />
           <Route path={"/menu"} element={<Menu />} />
         </Routes>
       </BrowserRouter>
